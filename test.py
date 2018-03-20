8504d5b67a8c8c07649040d4b1ee325473a01c5c
MariosPitsali <marios.pitsali@gmail.com>
2018-03-20 02:56:21 +0200

    adventure classes, enemy, troll

--- test.py
+++ test.py
@@ -1,13 +1,29 @@
-from characters import Player
+# from characters import Player
 
-jordan = Player("marios")
-print (jordan.lives)
-print (jordan.level)
-#takes advantage of __str__ function
-print (jordan)
+# jordan = Player("marios")
+# print (jordan.lives)
+# print (jordan.level)
+# #takes advantage of __str__ function
+# print (jordan)
 
-jordan._set_lives(5)
-print (jordan)
+# jordan._set_lives(5)
+# print (jordan)
 
-jordan._set_lives(-1)
-print (jordan)
+# jordan._set_lives(-1)
+# print (jordan)
+
+from enemy import Enemy, Troll
+
+random_monster = Enemy("Basic Enemy", 12, 1)
+random_monster.take_damage(4)
+random_monster.take_damage(9)
+print (random_monster)
+
+# ugly_troll = Troll()
+# print ("Ugly Troll - {}".format(ugly_troll))
+
+another_troll = Troll("Ug")
+print ("Another troll - {}".format(another_troll))
+
+brother = Troll("Urg")
+print ("Brother troll - {}".format(brother))
