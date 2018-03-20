b342ab3bd0f93de605194b6b1a0eed38f4f9b58b
MariosPitsali <marios.pitsali@gmail.com>
2018-03-20 17:51:50 +0200

    vampyre, troll added

--- enemy.py
+++ enemy.py
@@ -1,9 +1,35 @@
+
 class Enemy:
     
     def __init__(self, name="Enemy", hit_points=0, lives=1):
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
+        
+    def take_damage(self, damage):
+        remaining_points = self.hit_points - damage
+        if remaining_points >= 0:
+            self.hit_points = remaining_points
+            print("I took {} points of damage and have {} left".format(damage, self.hit_points))
+        else:
+            self.lives -= 1
+            if self.lives > 0 :
+                print ("{0.name} lost a life".format(self))
+            else:
+                print("{0.name} is dead".format(self))
+                self.alive = False
+
+    def __str__(self):
+        return ("Name: {0.name}, Lives: {0.lives}, Hit Points: {0.hit_points}".format(self))
+
+class Troll(Enemy):
+    
+    def __init__(self, name):
+        super().__init__(name=name, hit_points=23)
+        
+    def grunt(self):
+        print ("I am {0.name}. {0.name} gonna beat you.".format(self))
         
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
@@ -14,10 +40,29 @@ class Enemy:
             self.hit_points = 0
             self.lives -= 1
 
-    def __str__(self):
-        return ("Name: {0.name}, Lives: {0.lives}, Hit Points: {0.hit_points}".format(self))
-
-class Troll(Enemy):
+class Vampyre(Enemy):
+    """Class that represents Vampyre type enemies.
+    Vampyres are a subclass of class Enemy.
+    """
+    def __init__(self,name):
+        super().__init__(name=name, lives=3, hit_points=12)
+        
+    def dodges(self):
+        import random
+        if random.randint(1,3) == 3:
+            print ("****{0.name} dodges****".format(self))
+            return True
+        else:
+            return False
     
-    def __init__(self, name):
-        super().__init__(name=name, hit_points=23)
+    def take_damage(self, damage):
+        if not self.dodges():
+            super().take_damage(damage=damage)
+    
+    
+    
+    
+    
+    
+    
+    
--- test.py
+++ test.py
@@ -12,7 +12,7 @@
 # jordan._set_lives(-1)
 # print (jordan)
 
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampyre
 
 random_monster = Enemy("Basic Enemy", 12, 1)
 random_monster.take_damage(4)
@@ -24,6 +24,17 @@ print (random_monster)
 
 another_troll = Troll("Ug")
 print ("Another troll - {}".format(another_troll))
-
+another_troll.take_damage(18)
 brother = Troll("Urg")
 print ("Brother troll - {}".format(brother))
+vlad = Vampyre("Vlad")
+vlad.take_damage(5)
+print (vlad)
+
+print ("*" * 40)
+another_troll.take_damage(30)
+print (another_troll)
+
+while vlad.alive:
+    vlad.take_damage(1)
+    print (vlad)
