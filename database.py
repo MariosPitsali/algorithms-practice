58dec870697a333fa67cd31452772e38edd675df
MariosPitsali <marios.pitsali@gmail.com>
2018-03-24 19:02:40 +0200

    databases

---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+db = sqlite3.connect("contacts.sqlite")
+db.execute("create table IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("INSERT INTO contacts (name, phone, email) VALUES ('Tim', 123456, 'tim@gmail.com')")
+db.execute("INSERT INTO contacts VALUES ('Marios', 654321, 'marios@gmail.com')")
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM contacts")
+#prints a list of all tuples that cursor object returns
+lst = cursor.fetchall()
+for result_set in lst:
+    print (result_set)
+    print ("_" * 20)
+#prints empty list if cursor has been run through already
+print (cursor.fetchall())
+
+for name, phone, email in cursor:
+    print (name, phone, email, sep="-")
+    print ("*" * 40)
+db.close()
+#querying the database with python
+
+
+
