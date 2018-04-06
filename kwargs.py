0df0d739d497a742d86c15eacf8200ae7b7f8ed0
MariosPitsali <marios.pitsali@gmail.com>
2018-04-06 03:47:44 +0300

    fixed

--- kwargs.py
+++ kwargs.py
@@ -1,6 +1,10 @@
 #kwargs comes from key word arguments
-def print_backwards(*args):
+def print_backwards(*args, **kwargs):
+    print (kwargs)
     for arg in args[::-1]:
-        print (arg[::-1], end = ' ')
-
-print (print_backwards("awesome", "jello", "shot"))
+        if 'end' not in kwargs.keys():
+            print (arg[::-1], end= " ", **kwargs)
+        else:
+            print (arg[::-1], **kwargs)
+with open("backwards.txt", 'w') as backwards:
+    print (print_backwards("awesome", "jello", "shot", end= "\n"))
